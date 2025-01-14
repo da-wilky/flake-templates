d8e9f30b6978b13a950a2cf2d8e232ee9afbf58e
Samuel Wilk <34423885+da-wilky@users.noreply.github.com>
2025-01-14 16:56:34 +0100

    Update flake.nix

--- templates/customshell/flake.nix
+++ templates/customshell/flake.nix
@@ -11,16 +11,16 @@ rec {
     in
     {
       devShells.x86_64-linux.default = pkgs.mkShell {
-	packages = with pkgs; [
-	  # Packages made available on the CLI
-	];
+        packages = with pkgs; [
+          # Packages made available on the CLI
+        ];
 
-	shellHook = ''
-	  echo
-  	  echo
-	  echo
-	  echo -e "\033[1;31mWelcome to the ${description} environment!"
-	'';
+        shellHook = ''
+          echo
+          echo
+          echo
+          echo -e "\033[1;31mWelcome to the ${description} environment!"
+        '';
       };
     };
 }
